2b1cbfa95d128a006055b0229db25dbe48332192
Seth-Rah <seth.evangelion.rah@gmail.com>
2019-09-15 14:11:20 +0200

    Testing new message types and responses.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const connection = require('./classes/mysql');
 const telegramBot = require('node-telegram-bot-api');
 const telegram = new telegramBot(env.TOKEN, { polling: true });
 
-telegram.on("text", (message) => {
-  telegram.sendMessage(message.chat.id, "Brei is Gay, also I only accept posts with images or albums.");
-});
-
-telegram.on("photo", (message) => {
-  telegram.sendMessage(message.chat.id, "Brei is Gay, also thanks for the image response.");
+telegram.on('message', (message) => {
+  message = Array.isArray(message) ? message : [message]
+  if (!message.photo){
+    telegram.sendMessage(message.chat.id, "Brei is Gay, also I only accept posts with images or albums.");
+  } else if (message.photo.length > 1) {
+    telegram.sendMessage(message.chat.id, "Brei is Gay, also thanks for the album.");
+  } else {
+    telegram.sendMessage(message.chat.id, "Brei is Gay, also thanks for the image.");
+  }
 });
 
 telegram.on('polling_error', (error) => {
