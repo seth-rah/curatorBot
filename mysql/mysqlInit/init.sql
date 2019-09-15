53f5136f029ca0ca67f5778cc24821db98b1ae7d
Seth-Rah <seth.evangelion.rah@gmail.com>
2019-09-15 12:55:46 +0200

    init.sql alter Altered init.sql to better reflect messages received. Changed data types to recommended data types in telegram api documentation.

--- mysql/mysqlInit/init.sql
+++ mysql/mysqlInit/init.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `bannedusers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(64) unsigned NOT NULL AUTO_INCREMENT,
+  `chatID` bigint(64) NOT NULL,
+  `messageID` bigint(64) NOT NULL,
+  `userID` bigint(64) NOT NULL,
   `userName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `userFirstName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `userLastName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `postID` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
@@ -12,11 +13,17 @@ CREATE TABLE `bannedusers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `images` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `postID` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `imageID` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Status` enum('PENDING','REJECTED','APPROVED','POSTED') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(64) unsigned NOT NULL AUTO_INCREMENT,
+  `chatID` bigint(64) NOT NULL,
+  `messageID` bigint(64) NOT NULL,
+  `userID` bigint(64) NOT NULL,
+  `userName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `userFirstName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userLastName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fileID` bigint(64) NOT NULL,
+  `status` enum('PENDING','REJECTED','APPROVED','POSTED') COLLATE utf8mb4_unicode_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `imageID_UNIQUE` (`imageID`)
+  UNIQUE KEY `imageID_UNIQUE` (`fileID`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
