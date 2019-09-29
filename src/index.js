e03805407cb338b95f0154319a5cfb49128bfdee
Seth-Rah <seth.evangelion.rah@gmail.com>
2019-09-29 16:05:55 +0200

    Oops

--- src/index.js
+++ src/index.js
@@ -52,16 +52,3 @@ telegram.on('polling_error', (error) => {
     console.log("Please ensure that your token from t.me/botfather is set up correctly in the 'TOKEN' environment variable");
   } 
 });
-
-`id` bigint(64) unsigned NOT NULL AUTO_INCREMENT,
-`chatID` bigint(64) NOT NULL,
-`mediaGroupID` bigint(64) DEFAULT NULL,
-`messageID` bigint(64) NOT NULL,
-`userID` bigint(64) NOT NULL,
-`userName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-`userFirstName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-`userLastName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-`fileID` bigint(64) NOT NULL,
-`status` enum('PENDING','REJECTED','APPROVED','POSTED') COLLATE utf8mb4_unicode_ci NOT NULL,
-`timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`imagescol` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
